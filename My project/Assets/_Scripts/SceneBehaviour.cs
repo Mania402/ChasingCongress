f208439e0bd45abc1a633d3c8397d84b66fecb42
Jorge-7-X <jel367@pitt.edu>
2026-02-18 21:49:39 -0500

    Titlescreen settings menu
    In progress of changing sound settings and keybindings

--- My project/Assets/_Scripts/SceneBehaviour.cs
+++ My project/Assets/_Scripts/SceneBehaviour.cs
@@ -1,11 +1,33 @@
+using PixelCrushers;
+using PixelCrushers.DialogueSystem;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneBehaviour : MonoBehaviour
 {
+    [SerializeField] GameObject _mainMenu;
+    [SerializeField] GameObject _settingsMenu;
+
+    bool showSettings;
+
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void showSettingsMenu()
+    {
+        _settingsMenu.gameObject.SetActive(true);
+        showSettings = true;
+        _mainMenu.gameObject.SetActive(false);
+    }
+
+    public void hideSettingsMenu()
+    {
+        _settingsMenu.gameObject.SetActive(false);
+        showSettings = false;
+        _mainMenu.gameObject.SetActive(true);
     }
 
     public void QuitGame()
